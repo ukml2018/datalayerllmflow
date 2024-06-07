a8a601ee353c9e545b95c4d48528b47d9f5c1955
Uttam Kumar Manna <45291428+ukml2018@users.noreply.github.com>
2024-06-07 16:37:35 +0530

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -41,6 +41,7 @@ def create_dataframes(connection):
         product_data_query = "SELECT manufacturer_group, product_group, product_identity, product_name FROM product_data_en"
         product_data_df = pd.read_sql(sql=product_data_query, con=connection)
         r.set("product_data_df", pickle.dumps(product_data_df))
+        print("Loaded to Redis.")
     
     ib_dataframes.append(product_data_df)
 
@@ -64,6 +65,7 @@ def create_dataframes(connection):
         ff_dashboard_outlet_mapping_query = "select outlet_id, outlet_address, customer_number, area_code, region_code, micro_region_desc from ff_dashboard_outlet_mapping_en"
         ff_dashboard_outlet_mapping_df = pd.read_sql(sql=ff_dashboard_outlet_mapping_query, con=connection)
         r.set("ff_dashboard_outlet_mapping_df", pickle.dumps(ff_dashboard_outlet_mapping_df))
+        print("Loaded to Redis.")
     
     ib_dataframes.append(ff_dashboard_outlet_mapping_df)
 
@@ -73,30 +75,36 @@ def create_dataframes(connection):
         print("reconnect_export_df : I read from Redis.")
     
     except:
-        reconnect_export_query = "select product_identity, region, outlet_id, MVIS_outlet_id, transaction_date, measure_sales_volume, product_group, product_type, region_code from reconnect_export_en"
+        print("Reloading the table to cache.....")
+        reconnect_export_query = "select product_identity, region, outlet_id, transaction_date, measure_sales_volume, product_group, product_type, region_code from reconnect_export_en"
         reconnect_export_df = pd.read_sql(sql=reconnect_export_query, con=connection)
         r.set("reconnect_export_df", pickle.dumps(reconnect_export_df))
+        print("Loaded to Redis.")
     
     ib_dataframes.append(reconnect_export_df)
-    #try:
-     #   ff_imperial_marketshare_outlet_df= pd.DataFrame(pickle.loads(r.get("ff_imperial_marketshare_outlet_df")))
-      #  print("ff_imperial_marketshare_outlet_df : I read from Redis.")
+
+    try:
+        rc_outlet_marketshare_vw_df= pd.DataFrame(pickle.loads(r.get("rc_outlet_marketshare_vw_df")))
+        print("rc_outlet_marketshare_vw_df : I read from Redis.")
     
-    #except:
-     #   ff_imperial_marketshare_outlet_query = "select * from ff_imperial_marketshare_outlet_vw"
-      #  ff_imperial_marketshare_outlet_df = pd.read_sql(sql=ff_imperial_marketshare_outlet_query, con=connection)
-        #r.set("ff_imperial_marketshare_outlet_df", pickle.dumps(ff_imperial_marketshare_outlet_df))
+    except:
+        rc_outlet_marketshare_vw_query = "select * from rc_outlet_marketshare_vw"
+        rc_outlet_marketshare_vw_df = pd.read_sql(sql=rc_outlet_marketshare_vw_query, con=connection)
+        r.set("rc_outlet_marketshare_vw_df", pickle.dumps(rc_outlet_marketshare_vw_df))
+        print("Loaded to Redis.")
     
-    #ib_dataframes.append(ff_imperial_marketshare_outlet_df)
+    ib_dataframes.append(rc_outlet_marketshare_vw_df)
 
     try:
         distribution_tracking_df = pd.DataFrame(pickle.loads(r.get("distribution_tracking_df")))
         print("distribution_tracking_df : I read from Redis.")
     
     except:
-        distribution_tracking_query = "select customer_number, customer_address, area_code, location, region_code, sales_line_code, outlet_ID, focus_area, current_customer, date, product_identity, product_desc, product_in_stock, product_out_of_stock, micro_region, micro_region_desc, Power_Rank from distribution_tracking_en"
+        print("Let's reload the table again to cache:")
+        distribution_tracking_query = "SELECT outlet_id, product_identity, product_desc, product_in_stock, product_out_of_stock, micro_region FROM distribution_tracking_en"
         distribution_tracking_df= pd.read_sql(distribution_tracking_query,connection)
         r.set("distribution_tracking_df", pickle.dumps(distribution_tracking_df))
+        print("Loaded to Redis.")
         
     ib_dataframes.append(distribution_tracking_df)
     
